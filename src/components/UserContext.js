140535e3705d6c4f6e6a612ecb23d7fa3bf731fe
chayareem <113825594+chayareem@users.noreply.github.com>
2023-02-15 21:01:31 +0200

    Update UserContext.js

--- src/components/UserContext.js
+++ src/components/UserContext.js
@@ -4,20 +4,8 @@ export const userContext = createContext();
 
 export default function UserContext(props) {
 
-
     const [userState, setUserState] = useState({ FirstName: '', LastName: '', UserId: '', DateOfBirth: '', maleOrFemale: null, Hmo: null })
     const [childArr, setChildArr] = useState([]);
-
-    // const [userIDNumber, setUserIDNumber] = useState();
-    // const [userFirstname, setUserFirstname] = useState();
-    // const [userLastname, setUserLastname] = useState();
-    // const [userDate, setUserDate] = useState();
-    // const [userGender, setUserGender] = useState();
-    // const [userHMO, setUserHMO] = useState();
-    // const [childIDNumber, setChildIDNumber] = useState();
-    // const [childName, setChildName] = useState();
-    // const [childDate, setChikdDate] = useState();
-    // const [childrenArr,setChildrenArr]=useState();
 
     return (
         <userContext.Provider value={{ userState, setUserState, childArr, setChildArr }}>
